7b18fffcabd5cf556f6fb2e1dfd76634a762d9c2
andrejvysny <vysnyandrej@gmail.com>
2024-04-07 00:10:59 +0200

    env test

--- src/utils/Env.ts
+++ src/utils/Env.ts
@@ -2,9 +2,6 @@
 
 export default function getEnv(envName:string): string|undefined{
 
-    if (import.meta.env.PROD && import.meta.env.VITE_DOCKER){
-        return window?._env_?.[envName] ?? import.meta.env[envName] ?? undefined;
-    }
+    return window?._env_?.[envName] ?? import.meta.env[envName] ?? undefined;
 
-    return import.meta.env[envName] ?? undefined;
 }
